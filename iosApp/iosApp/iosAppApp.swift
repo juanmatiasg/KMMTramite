bd5755fe1026e5938f2270dbd320dcf1b8fc5116
Juan Guerrero <=>
2024-06-23 21:21:28 -0300

    Changing permission of gradlew

---
+++ iosApp/iosApp/iosAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  iosAppApp.swift
+//  iosApp
+//
+//  Created by Juan Guerrero on 23/06/2024.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct iosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
